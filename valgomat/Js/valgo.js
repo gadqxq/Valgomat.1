1764839dc03e66529928d5cd6e1563b1ae19358b
gadqxq <144331067+gadqxq@users.noreply.github.com>
2023-09-19 13:51:34 +0200

    fixed my dry ass code

--- valgomat/Js/valgo.js
+++ valgomat/Js/valgo.js
@@ -1,52 +1,120 @@
-        // JavaScript for å vise ett spørsmål om gangen
-        let currentQuestion = 1;
-        const totalQuestions = 10;
+    // Definerer spørsmål og svaralternativer
+    const questions = [
+        {
+            question: "Spørsmål 1: Hva er din mening om klimaendringer?",
+            options: [
+                { text: "Meget uenig", value: 1 },
+                { text: "Uenig", value: 2 },
+                { text: "Nøytral", value: 3 },
+                { text: "Enig", value: 4 },
+                { text: "Meget enig", value: 5 },
+            ],
+        },
+        {
+            question: "Spørsmål 1: Hva er din mening om klimaendringer?",
+            options: [
+                { text: "Meget uenig", value: 1 },
+                { text: "Uenig", value: 2 },
+                { text: "Nøytral", value: 3 },
+                { text: "Enig", value: 4 },
+                { text: "Meget enig", value: 5 },
+            ],
+        },
+        {
+            question: "Spørsmål 1: Hva er din mening om klimaendringer?",
+            options: [
+                { text: "Meget uenig", value: 1 },
+                { text: "Uenig", value: 2 },
+                { text: "Nøytral", value: 3 },
+                { text: "Enig", value: 4 },
+                { text: "Meget enig", value: 5 },
+            ],
+        },
+        {
+            question: "Spørsmål 1: Hva er din mening om klimaendringer?",
+            options: [
+                { text: "Meget uenig", value: 1 },
+                { text: "Uenig", value: 2 },
+                { text: "Nøytral", value: 3 },
+                { text: "Enig", value: 4 },
+                { text: "Meget enig", value: 5 },
+            ],
+        },
+    ];
 
-        function nextQuestion() {
-            const currentQuestionElement = document.getElementById(`question${currentQuestion}`);
-            const selectedOption = currentQuestionElement.querySelector('input:checked');
+    const quizForm = document.getElementById("quiz-form");
+    const quizQuestions = document.getElementById("quiz-questions");
+    const resultDiv = document.getElementById("result");
+    const startButton = document.getElementById("start-button");
+    const nextButton = document.getElementById("next-button");
+    let currentQuestionIndex = 0;
 
+    // Viser et spørsmål basert på indeksen
+    function showQuestion(index) {
+        const question = questions[index];
+        const questionElement = createQuestionElement(question, index);
+        quizQuestions.innerHTML = ""; // Tømmer forrige spørsmål
+        quizQuestions.appendChild(questionElement);
+    }
+
+    // Beregner resultatet basert på svarene
+    function calculateResult() {
+        let totalScore = 0;
+        for (let i = 0; i < questions.length; i++) {
+            const selectedOption = document.querySelector(`input[name="q${i}"]:checked`);
             if (selectedOption) {
-                // Skjul gjeldende spørsmål
-                currentQuestionElement.style.display = 'none';
-                currentQuestion++;
-
-                if (currentQuestion <= totalQuestions) {
-                    // Vis neste spørsmål
-                    document.getElementById(`question${currentQuestion}`).style.display = 'block';
-                }
-
-                // Skjul "Beregn resultat" knappen hvis vi ikke er på siste spørsmål
-                if (currentQuestion > totalQuestions) {
-                    document.getElementById('calculateButton').style.display = 'block';
-                }
-            } else {
-                alert('Vennligst svar på spørsmålet før du går videre.');
+                totalScore += parseInt(selectedOption.value);
             }
         }
+        const percentage = (totalScore / (questions.length * 5)) * 100;
+        resultDiv.textContent = `Du er ${percentage.toFixed(2)}% enig med Pensjonistpartiet`;
+    }
 
-        function calculateResult() {
-            // Implementer resultatberegning som tidligere
-            const questions = document.querySelectorAll('.question');
-            let totalScore = 0;
+    // Legger til Eventistener for knapper og skjemaer
+    startButton.addEventListener("click", function () {
+        startButton.style.display = "none"; // Skjuler startknappen
+        nextButton.style.display = "block"; // Viser neste knapp
+        showQuestion(currentQuestionIndex); // Viser det første spørsmålet
+        quizForm.style.display = "block"; // Viser skjemaet for spørsmål
+    });
 
-            questions.forEach((question, index) => {
-                const selectedOption = question.querySelector('input:checked');
-                if (selectedOption) {
-                    totalScore += parseInt(selectedOption.value);
-                }
-            });
-
-            const maxScore = totalQuestions * 4;
-            const agreementPercentage = (totalScore / maxScore) * 100;
-
-            const resultElement = document.getElementById('result');
-            resultElement.textContent = `Du er ${agreementPercentage.toFixed(2)}% enig.`;
-
-            // Skjul "Neste spørsmål" knappen og vis resultatet
-            document.getElementById('calculateButton').style.display = 'none';
-            resultElement.style.display = 'block';
+    quizForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        currentQuestionIndex++; // Går til neste spørsmål
+        if (currentQuestionIndex < questions.length) {
+            showQuestion(currentQuestionIndex); // Viser neste spørsmål
+            nextButton.disabled = true; // Deaktiverer neste-knappen igjen
+        } else {
+            calculateResult(); // Beregner resultatet når alle spørsmålene er besvart
+            quizQuestions.style.display = "none"; // Skjuler spørsmålene
+            nextButton.style.display = "none"; // Skjuler neste-knappen
+            resultDiv.style.display = "block"; // Viser resultatet
         }
+    });
 
-        // Vis det første spørsmålet ved oppstart
-        document.getElementById('question1').style.display = 'block';
+    // Aktiverer "Neste" knappen når et alternativ er valgt
+    quizForm.addEventListener("change", function () {
+        nextButton.disabled = false;
+    });
+
+    // Genererer spørsmålselementer
+    function createQuestionElement(questionObj, index) {
+        const questionElement = document.createElement("div");
+        questionElement.innerHTML = `
+            <p>${questionObj.question}</p>
+            <div style="display: flex; flex-direction: row;">
+                ${questionObj.options
+                    .map(
+                        (option, optionIndex) =>
+                            `<input type="radio" name="q${index}" value="${option.value}" id="q${index}o${optionIndex}">
+                            <label for="q${index}o${optionIndex}">${option.text}</label>`
+                    )
+                    .join("")}
+            </div>
+        `;
+        return questionElement;
+    }
+
+    // Skjuler spørsmålskjemaet og "Neste" knappen i begynnelsen
+    quizForm.style.display = "none";
+    nextButton.style.display = "none";
